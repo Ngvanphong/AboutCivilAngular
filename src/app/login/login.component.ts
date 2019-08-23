445854a0fed285ca322ffefe996e6160b1e1d080
Nguyen van Phong <ngvanphong2012@gmail.com>
2019-08-23 11:29:43 +0700

    654

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenService} from '../core/service/authen.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public loading=false;
+  public model:any={};
+  constructor(private _authenService:AuthenService) { }
 
   ngOnInit() {
+  }
+  public login(){
+    this.loading=true;  
   }
 
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import {Routes,RouterModule} from '@angular/router'
-import { from } from 'rxjs';
-
+import{FormsModule} from '@angular/forms';
+import { AuthenService } from '../core/service/authen.service';
 const loginRoutes:Routes=[
   {path:'',redirectTo:'index',pathMatch:'full'},
   {path:'index',component:LoginComponent}
@@ -12,7 +12,12 @@ const loginRoutes:Routes=[
   declarations: [LoginComponent],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(loginRoutes)
+  ],
+  providers:[
+    AuthenService
   ]
+  
 })
 export class LoginModule { }
