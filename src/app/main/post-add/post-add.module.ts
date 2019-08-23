f79f42f9fc032bb3c5cd400009b51076abc3cfd0
Nguyen van Phong <ngvanphong2012@gmail.com>
2019-08-23 08:48:47 +0700

    123

---
+++ src/app/main/post-add/post-add.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostAddComponent } from './post-add.component';
+
+
+
+@NgModule({
+  declarations: [PostAddComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PostAddModule { }
---
+++ src/app/main/system-config/system-config.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SystemConfigComponent } from './system-config.component';
+
+
+
+@NgModule({
+  declarations: [SystemConfigComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SystemConfigModule { }
