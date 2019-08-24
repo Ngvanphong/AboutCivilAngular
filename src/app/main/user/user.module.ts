afbecc71b05918254137d5e42c676edbc7de94a8
Nguyen van Phong <ngvanphong2012@gmail.com>
2019-08-24 11:31:51 +0700

    365

--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
+import {Routes,RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import{NgMultiSelectDropDownModule} from 'ng-multiselect-dropdown';
+import{BsDatepickerModule, PaginationModule} from 'ngx-bootstrap';
+import {ModalModule} from 'ngx-bootstrap';
 
-
+const userRouter:Routes=[
+  {path:'',redirectTo:'index',pathMatch:'full'},
+  {path:'index',component:UserComponent}
+]
 
 @NgModule({
   declarations: [UserComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(userRouter),
+    ModalModule.forRoot(),
+    PaginationModule.forRoot(),
+    NgMultiSelectDropDownModule.forRoot(),
+    BsDatepickerModule.forRoot(),
   ]
 })
 export class UserModule { }
