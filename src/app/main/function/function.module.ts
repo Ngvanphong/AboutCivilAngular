724ed017c5f142aa3c23d10b9bb3ae8953cc32ff
Nguyen van Phong <ngvanphong2012@gmail.com>
2019-09-10 16:25:16 +0700

    215

--- src/app/main/function/function.module.ts
+++ src/app/main/function/function.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FunctionComponent } from './function.component';
-
-
+import {Routes,RouterModule} from '@angular/router';
+import {ModalModule} from 'ngx-bootstrap';
+import {FormsModule} from '@angular/forms';
+import {TreeModule} from 'angular-tree-component'
+const routerFunction:Routes=[
+  {path:'',redirectTo:'index',pathMatch:'full'},
+  {path:'index',component:FunctionComponent}
+]
 
 @NgModule({
   declarations: [FunctionComponent],
-  imports: [
-    CommonModule
+  imports: [ 
+    CommonModule,
+    RouterModule.forChild(routerFunction),
+    FormsModule,
+    ModalModule.forRoot(),
+    TreeModule.forRoot()
   ]
 })
 export class FunctionModule { }
